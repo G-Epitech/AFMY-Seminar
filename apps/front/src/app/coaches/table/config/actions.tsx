547527487a20befd2f00cd1e5100fc2e7dfe9117
Yann <yannmasson17@gmail.com>
2024-09-03 23:13:44 +0200

    feat: add responsive in table filters

--- apps/front/src/app/coaches/table/config/actions.tsx
+++ apps/front/src/app/coaches/table/config/actions.tsx
@@ -7,35 +7,38 @@ import { useRouter } from "next/navigation";
 
 export const actionsColumn: ColumnDef<Coach> = {
   id: "actions",
+  header: () => (<div className="text-right">Actions</div>),
   enableHiding: false,
   cell: ({ row }) => {
     const coach = row.original;
     const router = useRouter();
 
     return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <DotsHorizontalIcon className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <DropdownMenuItem
-            onClick={() => router.push(`/coaches/${coach.id}`)}
-          >
-            view profile
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => navigator.clipboard.writeText(coach.email)}
-          >
-            Copy coach email
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Promote</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="flex items-center justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => router.push(`/coaches/${coach.id}`)}
+            >
+              view profile
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(coach.email)}
+            >
+              Copy coach email
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Promote</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     )
   },
 }
--- apps/front/src/app/coaches/table/filter.tsx
+++ apps/front/src/app/coaches/table/filter.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
-import { ChevronDownIcon } from "@radix-ui/react-icons"
+import { ChevronDownIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons"
 import { useReactTable } from "@tanstack/react-table";
 import { Coach } from "@/types/coach";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 
 export interface CoachesTableFiltersProps {
   table: ReturnType<typeof useReactTable<Coach>>;
@@ -12,42 +14,66 @@ export interface CoachesTableFiltersProps {
 export default function CoachesTableFilters(
   { table }: CoachesTableFiltersProps
 ) {
+  const [filter, setFilter] = useState<Boolean>(false);
+
+  const toggleFilter = () => {
+    setFilter(prev => !prev);
+  }
+
+  const inputFilter = (
+    <Input
+      placeholder="Filter emails..."
+      value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+      onChange={(event) =>
+        table.getColumn("email")?.setFilterValue(event.target.value)
+      }
+      className="max-w-sm"
+    />
+  );
+
   return <>
-    <div className="flex items-center py-4">
-      <Input
-        placeholder="Filter emails..."
-        value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-        onChange={(event) =>
-          table.getColumn("email")?.setFilterValue(event.target.value)
-        }
-        className="max-w-sm"
-      />
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" className="ml-auto">
-            Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          {table
-            .getAllColumns()
-            .filter((column) => column.getCanHide())
-            .map((column) => {
-              return (
-                <DropdownMenuCheckboxItem
-                  key={column.id}
-                  className="capitalize"
-                  checked={column.getIsVisible()}
-                  onCheckedChange={(value) =>
-                    column.toggleVisibility(!!value)
-                  }
-                >
-                  {column.id}
-                </DropdownMenuCheckboxItem>
-              )
-            })}
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <div className="flex flex-wrap items-center justify-center py-4 gap-3">
+      <div className="sm:ml-auto flex gap-2 w-full sm:w-auto justify-center flex-row items-center space-x-4 text-sm">
+        <div className="flex flex-row-reverse items-center space-x-4">
+          <MagnifyingGlassIcon
+            className="h-4 w-4 ms-4"
+            onClick={toggleFilter}
+          />
+          <div className="hidden sm:block">
+            {filter && inputFilter}
+          </div>
+        </div>
+        <Separator orientation="vertical" className="h-5" />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                )
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="block sm:hidden">
+        {filter && inputFilter}
+      </div>
     </div>
   </>
 }
