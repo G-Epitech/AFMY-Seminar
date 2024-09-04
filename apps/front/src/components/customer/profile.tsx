2d3023c5445edd82429c8a00f11e04efe392d1a9
Matheo Coquet <matheo.coquet@epitech.eu>
2024-09-04 11:04:09 +0200

    feat: update customer profile

--- apps/front/src/components/customer/profile.tsx
+++ apps/front/src/components/customer/profile.tsx
@@ -2,14 +2,31 @@ import { Customer, Employee } from "@seminar/common";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Separator } from "../ui/separator";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 
-function ShortDetail({ label, value }: { label: string; value: string }) {
+function ShortDetail({
+    label,
+    value,
+    link,
+}: {
+    label: string;
+    value: string;
+    link?: string;
+}) {
     return (
         <div className="flex flex-col text-sm py-2 w-[70%]">
             <p className="text-gray-500">{label}:</p>
-            <p>{value}</p>
+
+            <div className="flex items-center gap-1">
+                <p>{value}</p>
+                {link && (
+                    <a href={link}>
+                        <ArrowTopRightOnSquareIcon className="size-4 stroke-[2px] stroke-primary" />
+                    </a>
+                )}
+            </div>
         </div>
-    )
+    );
 }
 
 export function CustomerProfile({
@@ -33,7 +50,12 @@ export function CustomerProfile({
                     <AvatarFallback>{customer.name[0]}</AvatarFallback>
                 </Avatar>
 
-                <h3 className="font-semibold text-lg">{customer.name}</h3>
+                <div>
+                    <h3 className="font-semibold text-lg">{customer.name}</h3>
+                    <p className="text-sm font-light text-gray-500">
+                        {customer.description}
+                    </p>
+                </div>
             </CardHeader>
 
             <CardContent>
@@ -44,7 +66,9 @@ export function CustomerProfile({
                         <h3 className="font-semibold text-lg">
                             {totalEncounters}
                         </h3>
-                        <p className="text-gray-500 text-sm">Total Encounters</p>
+                        <p className="text-gray-500 text-sm">
+                            Total Encounters
+                        </p>
                     </div>
 
                     <div className="flex flex-col gap-1 items-center">
@@ -66,10 +90,21 @@ export function CustomerProfile({
 
                 <h3 className="text-sm font-semibold mb-2">Short details</h3>
                 <ShortDetail label="User ID" value={customer.id.toString()} />
-                <ShortDetail label="Email" value={customer.email} />
-                <ShortDetail label="Address" value={customer.address || "Info unavailable"} />
+                <ShortDetail
+                    label="Email"
+                    value={customer.email}
+                    link={"mailto:" + customer.email}
+                />
+                <ShortDetail
+                    label="Address"
+                    value={customer.address || "Info unavailable"}
+                />
                 <ShortDetail label="Last Activity" value={"Info unavailable"} />
-                <ShortDetail label="Coach" value={coach.name} />
+                <ShortDetail
+                    label="Coach"
+                    value={coach.name}
+                    link={"/coachs/" + coach.id}
+                />
             </CardContent>
         </Card>
     );
