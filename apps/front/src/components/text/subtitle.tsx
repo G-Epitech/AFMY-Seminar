ed842ac55bccd194911fee818ee5b6ae3c31a6e5
Matheo Coquet <matheo.coquet@epitech.eu>
2024-09-04 14:10:05 +0200

    fix: texts errors

--- apps/front/src/components/text/subtitle.tsx
+++ apps/front/src/components/text/subtitle.tsx
@@ -1,10 +1,11 @@
 export function Subtitle({ text }: { text: string }) {
+    const splitText = text.split(" ");
     return (
         <div className="text-3xl font-extrabold tracking-tight">
             <h1>
-                Customer{" "}
+                {splitText[0]}{" "}
                 <span className="text-transparent bg-gradient-to-r from-[#FCAF6F] to-[#F97316] bg-clip-text">
-                    Details
+                    {splitText[1]}
                 </span>
             </h1>
         </div>
