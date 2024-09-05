bdb1594f744e1d253f16dc71422c0e10f4509ace
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-09-05 23:10:24 +0200

    feat: Add statistics type

---
+++ packages/common/src/types/statistics.type.ts
@@ -0,0 +1,22 @@
+export type Statistics = {
+  customers: {
+    count: number; // Total number of customers in the time frame
+    evolution_count: number; // Percentage of customers evolution in the time frame compared to the previous one
+    doing_meetings: number; // Number of customers doing meetings in the time frame
+    evolution_doing_meetings: number; // Percentage of customers doing meetings evolution in the time frame compared to the previous one
+    customers_by_coach: number; // Average number of customers by coach in the time frame
+    history: [{ date: Date; count: number }]; // Number of customers by date in the time frame
+    history_previous: [{ date: Date; count: number }]; // Number of customers by date in the previous time frame
+    countries: [{ country: string; count: number; percentage: number }]; // Number of customers by country in the time frame
+  };
+  events: {
+    count_month: number; // Number of events in the 'sliding' month (30 days from the time frame's end)
+    evolution_count_month: number; // Percentage of events evolution in the 'sliding' month compared to the previous one
+    count_week: number; // Number of events in the 'sliding' week (7 days from the time frame's end)
+    evolution_count_week: number; // Percentage of events evolution in the 'sliding' week compared to the previous one
+    daily_average: number; // Average number of events per day in the time frame
+    evolution_daily_average: number; // Percentage of events evolution in the daily average compared to the previous one
+    history: [{ date: Date; count: number }]; // Number of events by date in the time frame
+  };
+  meetings: [{ source: string; count: number; percentage: number }]; // Number of meetings by source in the time frame
+};
