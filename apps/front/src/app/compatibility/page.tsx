35de94250fdfe31579d3b34e2f93505b8ab36762
Matheo Coquet <matheo.coquet@epitech.eu>
2024-09-05 14:01:37 +0200

    feat: add generator and components

---
+++ apps/front/src/app/compatibility/page.tsx
@@ -0,0 +1,14 @@
+import { AutoComplete } from "@/components/ui/autocomplete";
+import { generator } from "@/lib/generator";
+
+export default function CompatibilityPage() {
+    const customers = generator.customers();
+
+    return (
+        <main>
+            <div>
+                
+            </div>
+        </main>
+    );
+}
