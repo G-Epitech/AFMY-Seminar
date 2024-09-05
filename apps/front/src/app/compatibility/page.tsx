2f5e850ff30074163eb3260e6ee2d4b38a1d9722
Matheo Coquet <matheo.coquet@epitech.eu>
2024-09-05 23:41:10 +0200

    feat: add user selector for compatibility

--- apps/front/src/app/compatibility/page.tsx
+++ apps/front/src/app/compatibility/page.tsx
@@ -1,14 +1,44 @@
-import { AutoComplete } from "@/components/ui/autocomplete";
-import { generator } from "@/lib/generator";
+"use client";
+
+import { CompatibilitySelectUser } from "@/components/compatibility/select";
+import { Subtitle } from "@/components/text/subtitle";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Customer } from "@seminar/common";
+import { useState } from "react";
 
 export default function CompatibilityPage() {
-    const customers = generator.customers();
+    const [customerA, setCustomerA] = useState<Customer | null>(null);
+    const [customerB, setCustomerB] = useState<Customer | null>(null);
+
+    const [customers, setCustomers] = useState<Customer[]>([]);
+    const [isCustomerLoading, setIsCustomerLoading] = useState(true);
 
     return (
         <main>
-            <div>
-                
-            </div>
+            <Subtitle text="Astrological Compatibility" />
+
+            <Card className="mt-6">
+                <CardHeader>
+                    <div className="flex gap-10 items-center justify-center md:flex-row flex-col">
+                        <CompatibilitySelectUser
+                            customer={customerA}
+                            setCustomer={setCustomerA}
+                            customers={customers}
+                            isCustomerLoading
+                        />
+                        <i
+                            className="fa-sharp fa-solid fa-wave-pulse fa-2xl"
+                            style={{ color: "#f97316" }}
+                        />
+                        <CompatibilitySelectUser
+                            customer={customerB}
+                            setCustomer={setCustomerB}
+                            customers={customers}
+                            isCustomerLoading
+                        />
+                    </div>
+                </CardHeader>
+            </Card>
         </main>
     );
 }
