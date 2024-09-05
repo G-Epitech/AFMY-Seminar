69360c7d1398d3a21449f2f53d122ccaeb3b8d70
Yann <yannmasson17@gmail.com>
2024-09-05 23:05:16 +0200

    feat: add get customers api call

--- apps/front/src/api/customers/list.ts
+++ apps/front/src/api/customers/list.ts
@@ -2,9 +2,12 @@ import { OutGetCustomersDTO, QueryGetCustomersDTO } from "@seminar/common";
 import { call } from "../call";
 
 export function list(query: QueryGetCustomersDTO) {
-    return call<QueryGetCustomersDTO, OutGetCustomersDTO>(
-        "GET",
-        "/customers",
-        query
-    );
+  const filteredQuery = Object.fromEntries(
+    Object.entries(query).filter(([_, value]) => value !== undefined)
+  );
+
+  return call<QueryGetCustomersDTO, OutGetCustomersDTO>(
+    "GET",
+    `/customers?${new URLSearchParams(filteredQuery as Record<string, string>)}`,
+  );
 }
