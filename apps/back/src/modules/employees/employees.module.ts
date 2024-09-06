a2b0e5fb5a30ad0b8cdf1cc3dc542470a26035d6
Flavien Chenu <flavienchenu@hotmail.com>
2024-09-06 14:42:42 +0200

    refactor(back): update images link generations

--- apps/back/src/modules/employees/employees.module.ts
+++ apps/back/src/modules/employees/employees.module.ts
@@ -5,6 +5,7 @@ import { EmployeesMigrationService } from './employees-migration.service';
 import { LegacyApiModule } from '../../providers/legacy-api/legacy-api.module';
 import { AuthModule } from '../auth/auth.module';
 import { EmployeesController } from './employees.controller';
+import { ImagesModule } from '../images/images.module';
 
 const EmployeesService = {
   provide: OriginalEmployeesService,
@@ -12,7 +13,12 @@ const EmployeesService = {
 };
 
 @Module({
-  imports: [PrismaModule, LegacyApiModule, forwardRef(() => AuthModule)],
+  imports: [
+    PrismaModule,
+    ImagesModule,
+    LegacyApiModule,
+    forwardRef(() => AuthModule),
+  ],
   providers: [EmployeesService, EmployeesMigrationService],
   exports: [EmployeesService, EmployeesMigrationService],
   controllers: [EmployeesController],
