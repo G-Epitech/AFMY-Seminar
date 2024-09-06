6afbac11b38f96195f627d705c5b9e899038b6ad
Yann <yannmasson17@gmail.com>
2024-09-06 03:33:46 +0200

    fix: remove useless code

--- apps/front/src/app/customers/[id]/closet/page.tsx
+++ apps/front/src/app/customers/[id]/closet/page.tsx
@@ -19,13 +19,9 @@ export default function ClosetPage() {
     [ClotheType.SHOES]: null,
   });
 
-  const fetchCustomers = async () => {
-    const response = await api.customers.list();
-    console.log(response);
-    fetchCustomerClothes();
-  }
-
+  // This function must be updated in the future, after a talk with the backend team.
   const fetchCustomerClothes = async () => {
+    await api.customers.list();
     const response = await api.customers.clothes({ id: parseInt(id) });
     console.log(response);
     if (response) {
@@ -38,18 +34,8 @@ export default function ClosetPage() {
   }
 
   useEffect(() => {
-    // const updatedUrlsClothes = temporaryClothes.map(c => {
-    //   c.image = `https://picsum.photos/400/534?random=${c.id}`;
-    //   return c;
-    // });
-    // setClothes(updatedUrlsClothes);
-    fetchCustomers();
-    // fetchCustomerClothes();
+    fetchCustomerClothes();
   }, []);
-
-  useEffect(() => {
-    console.log(selectedItemIds);
-  }, [selectedItemIds]);
 
   return (
     <main>
@@ -89,84 +75,3 @@ export default function ClosetPage() {
     </main>
   );
 }
-
-const temporaryClothes: Clothe[] = [
-  {
-    id: 1,
-    legacyId: 1,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 2,
-    legacyId: 2,
-    type: ClotheType.TOP,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 3,
-    legacyId: 3,
-    type: ClotheType.HAT_CAP,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 4,
-    legacyId: 4,
-    type: ClotheType.SHOES,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 5,
-    legacyId: 5,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 6,
-    legacyId: 6,
-    type: ClotheType.TOP,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 7,
-    legacyId: 7,
-    type: ClotheType.HAT_CAP,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 8,
-    legacyId: 8,
-    type: ClotheType.SHOES,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 9,
-    legacyId: 9,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 10,
-    legacyId: 10,
-    type: ClotheType.TOP,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 11,
-    legacyId: 12,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 12,
-    legacyId: 12,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-  {
-    id: 13,
-    legacyId: 13,
-    type: ClotheType.BOTTOM,
-    image: "https://picsum.photos/400/534",
-  },
-];
