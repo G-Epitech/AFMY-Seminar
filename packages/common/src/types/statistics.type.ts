9742eb468184c99f0d2b09430c5e547b5438049c
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-09-06 10:56:44 +0200

    feat: Add statistics computation and endpoint

--- packages/common/src/types/statistics.type.ts
+++ packages/common/src/types/statistics.type.ts
@@ -1,3 +1,8 @@
+export type History = {
+  date: Date;
+  count: number;
+}[];
+
 export type Statistics = {
   customers: {
     count: number; // Total number of customers in the time frame
@@ -5,9 +10,9 @@ export type Statistics = {
     doing_meetings: number; // Number of customers doing meetings in the time frame
     evolution_doing_meetings: number; // Percentage of customers doing meetings evolution in the time frame compared to the previous one
     customers_by_coach: number; // Average number of customers by coach in the time frame
-    history: [{ date: Date; count: number }]; // Number of customers by date in the time frame
-    history_previous: [{ date: Date; count: number }]; // Number of customers by date in the previous time frame
-    countries: [{ country: string; count: number; percentage: number }]; // Number of customers by country in the time frame
+    history: History; // Number of customers by date in the time frame
+    history_previous: History; // Number of customers by date in the previous time frame
+    countries: { country: string; count: number; percentage: number }[]; // Number of customers by country in the time frame
   };
   events: {
     count_month: number; // Number of events in the 'sliding' month (30 days from the time frame's end)
@@ -16,7 +21,12 @@ export type Statistics = {
     evolution_count_week: number; // Percentage of events evolution in the 'sliding' week compared to the previous one
     daily_average: number; // Average number of events per day in the time frame
     evolution_daily_average: number; // Percentage of events evolution in the daily average compared to the previous one
-    history: [{ date: Date; count: number }]; // Number of events by date in the time frame
+    history: History; // Number of events by date in the time frame
   };
-  meetings: [{ source: string; count: number; percentage: number }]; // Number of meetings by source in the time frame
+  meetings: { source: string; count: number; percentage: number }[]; // Number of meetings by source in the time frame
+};
+
+export type TimeFrame = {
+  start: Date;
+  end: Date;
 };
