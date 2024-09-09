50bfd85e6908064544aa33ca35145b46047ab1ec
Yann <yannmasson17@gmail.com>
2024-09-09 20:21:01 +0200

    feat: fetch real data from api

--- apps/front/src/api/employees/index.ts
+++ apps/front/src/api/employees/index.ts
@@ -1,7 +1,9 @@
+import { list } from "./list";
 import { me } from "./me";
 
 const employees = {
     me,
+    list
 };
 
 export default employees;
--- apps/front/src/app/coaches/page.tsx
+++ apps/front/src/app/coaches/page.tsx
@@ -1,17 +1,51 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Employee, Gender, Page, Permission } from '@seminar/common';
 import CoachesTable from '@/components/coaches/table';
 import { Subtitle } from '@/components/text/subtitle';
+import api from '@/api';
+import { useToast } from '@/hooks/use-toast';
+import { ToastAction } from '@/components/ui/toast';
 
 export default function Coaches() {
+  const { toast } = useToast();
   const [coachesPage, setCoachesPage] = useState<Page<Employee>>({
     index: 0,
     size: 15,
     isLast: false,
-    items: temporaryCoaches,
+    items: [],
   });
-  const [numberOfCoaches, setNumberOfCoaches] = useState<number>(coachesPage.items.length);
+  const [numberOfCoaches, setNumberOfCoaches] = useState<number>(0);
+
+  const fecthCoaches = async (index: number, size: number) => {
+    const response = await api.employees.list({
+      page: index,
+      size,
+      permission: Permission.COACH
+    });
+    console.log(response);
+    if (response && response.data) {
+      setCoachesPage(response.data);
+    } else {
+      console.error("Failed to fetch coaches");
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        action: <ToastAction
+          altText="Try again"
+          onClick={() =>
+            fecthCoaches(coachesPage.index, coachesPage.size)
+          }
+        >
+          Try again
+        </ToastAction>,
+      })
+    }
+  };
+
+  useEffect(() => {
+    fecthCoaches(coachesPage.index, coachesPage.size);
+  }, []);
 
   return (
     <main>
@@ -24,231 +58,3 @@ export default function Coaches() {
     </main>
   );
 }
-
-const temporaryCoaches: Employee[] = [
-  {
-    id: 2,
-    email: "jane.smith@example.com",
-    name: "Jane",
-    surname: "Smith",
-    birthDate: new Date("1990-07-22"),
-    gender: Gender.FE,
-    phone: "234-567-8901",
-    photo: null,
-    address: "456 Elm St, Metropolis",
-    permission: Permission.COACH,
-    role: "Fitness Coach",
-    legacyId: 1,
-    numberOfCustomers: 20
-  },
-  {
-    id: 3,
-    email: "bob.johnson@example.com",
-    name: "Bob",
-    surname: "Johnson",
-    birthDate: new Date("1978-05-03"),
-    gender: Gender.MA,
-    phone: "345-678-9012",
-    photo: null,
-    address: "789 Oak St, Smallville",
-    permission: Permission.COACH,
-    legacyId: 2,
-    role: "Operations Manager"
-  },
-  {
-    id: 4,
-    email: "alice.brown@example.com",
-    name: "Alice",
-    surname: "Brown",
-    birthDate: new Date("1982-11-19"),
-    gender: Gender.FE,
-    phone: "456-789-0123",
-    photo: null,
-    address: "101 Maple St, Riverdale",
-    permission: Permission.COACH,
-    role: "Yoga Instructor",
-    legacyId: 3
-  },
-  {
-    id: 5,
-    email: "michael.white@example.com",
-    name: "Michael",
-    surname: "White",
-    birthDate: new Date("1995-02-28"),
-    gender: Gender.MA,
-    phone: "567-890-1234",
-    photo: null,
-    address: "202 Pine St, Hill Valley",
-    permission: Permission.COACH,
-    role: "Strength Coach",
-    legacyId: 4
-  },
-  {
-    id: 6,
-    email: "emma.green@example.com",
-    name: "Emma",
-    surname: "Green",
-    birthDate: new Date("1992-09-05"),
-    gender: Gender.FE,
-    phone: "678-901-2345",
-    photo: null,
-    address: "303 Cedar St, Gotham",
-    permission: Permission.COACH,
-    role: "HR Manager",
-    legacyId: 5
-  },
-  {
-    id: 7,
-    email: "william.thompson@example.com",
-    name: "William",
-    surname: "Thompson",
-    birthDate: new Date("1988-04-14"),
-    gender: Gender.MA,
-    phone: "789-012-3456",
-    photo: null,
-    address: "404 Birch St, Star City",
-    permission: Permission.COACH,
-    role: "Cardio Coach",
-    legacyId: 6
-  },
-  {
-    id: 8,
-    email: "olivia.jones@example.com",
-    name: "Olivia",
-    surname: "Jones",
-    birthDate: new Date("1987-08-23"),
-    gender: Gender.FE,
-    phone: "890-123-4567",
-    photo: null,
-    address: "505 Spruce St, Central City",
-    permission: Permission.COACH,
-    role: "Pilates Instructor",
-    legacyId: 7
-  },
-  {
-    id: 9,
-    email: "james.williams@example.com",
-    name: "James",
-    surname: "Williams",
-    birthDate: new Date("1984-12-02"),
-    gender: Gender.MA,
-    phone: "901-234-5678",
-    photo: null,
-    address: "606 Redwood St, Coast City",
-    permission: Permission.COACH,
-    role: "Sales Manager",
-    legacyId: 8
-  },
-  {
-    id: 10,
-    email: "sophia.miller@example.com",
-    name: "Sophia",
-    surname: "Miller",
-    birthDate: new Date("1993-10-11"),
-    gender: Gender.FE,
-    phone: "012-345-6789",
-    photo: null,
-    address: "707 Oakwood St, National City",
-    permission: Permission.COACH,
-    role: "Nutrition Coach",
-    legacyId: 9
-  },
-  {
-    id: 11,
-    email: "liam.moore@example.com",
-    name: "Liam",
-    surname: "Moore",
-    birthDate: new Date("1980-06-17"),
-    gender: Gender.MA,
-    phone: "123-456-7890",
-    photo: null,
-    address: "808 Willow St, Starling City",
-    permission: Permission.COACH,
-    role: "CrossFit Coach",
-    legacyId: 10
-  },
-  {
-    id: 12,
-    email: "isabella.davis@example.com",
-    name: "Isabella",
-    surname: "Davis",
-    birthDate: new Date("1994-01-29"),
-    gender: Gender.FE,
-    phone: "234-567-8901",
-    photo: null,
-    address: "909 Aspen St, Keystone City",
-    permission: Permission.COACH,
-    role: "Dance Instructor",
-    legacyId: 11
-  },
-  {
-    id: 13,
-    email: "noah.clark@example.com",
-    name: "Noah",
-    surname: "Clark",
-    birthDate: new Date("1991-03-08"),
-    gender: Gender.MA,
-    phone: "345-678-9012",
-    photo: null,
-    address: "1010 Fir St, Blüdhaven",
-    permission: Permission.COACH,
-    role: "Marketing Manager",
-    legacyId: 12
-  },
-  {
-    id: 14,
-    email: "mia.martin@example.com",
-    name: "Mia",
-    surname: "Martin",
-    birthDate: new Date("1989-11-06"),
-    gender: Gender.FE,
-    phone: "456-789-0123",
-    photo: null,
-    address: "1111 Cypress St, Fawcett City",
-    permission: Permission.COACH,
-    role: "Aerobics Instructor",
-    legacyId: 13
-  },
-  {
-    id: 15,
-    email: "ethan.rodriguez@example.com",
-    name: "Ethan",
-    surname: "Rodriguez",
-    birthDate: new Date("1996-07-15"),
-    gender: Gender.MA,
-    phone: "567-890-1234",
-    photo: null,
-    address: "1212 Hickory St, Midway City",
-    permission: Permission.COACH,
-    role: "Personal Trainer",
-    legacyId: 14
-  },
-  {
-    id: 16,
-    email: "ava.lee@example.com",
-    name: "Ava",
-    surname: "Lee",
-    birthDate: new Date("1997-12-24"),
-    gender: Gender.FE,
-    phone: "678-901-2345",
-    photo: null,
-    address: "1313 Sequoia St, Gateway City",
-    permission: Permission.COACH,
-    role: "Meditation Coach",
-    legacyId: 15
-  },
-  {
-    id: 17,
-    email: "logan.walker@example.com",
-    name: "Logan",
-    surname: "Walker",
-    birthDate: new Date("1986-09-18"),
-    gender: Gender.MA,
-    phone: "789-012-3456",
-    photo: null,
-    address: "1414 Sycamore St, Opal City",
-    permission: Permission.COACH,
-    role: "Finance Manager",
-    legacyId: 16
-  }
-];
