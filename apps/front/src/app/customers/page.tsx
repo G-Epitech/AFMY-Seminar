e47f6cd5da07213696f03dcbda82dcf0577f16d9
Yann <yannmasson17@gmail.com>
2024-09-09 16:34:57 +0200

    feat: create customers complete

--- apps/front/src/app/customers/page.tsx
+++ apps/front/src/app/customers/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import { AstrologicalSign, Customer, Gender, Page, PaymentMethod } from '@seminar/common';
+import React, { ChangeEvent, useState } from 'react';
+import { AstrologicalSign, Customer, Gender, Page, PaymentMethod, PhotoFormat } from '@seminar/common';
 import CustomersTable from '@/components/customers/table';
 import { Subtitle } from '@/components/text/subtitle';
 import { Button } from '@/components/ui/button';
@@ -12,9 +12,16 @@ import { DatePicker } from '@/components/ui/date-picker';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import mapData from '../../../public/world-map.json';
+import { useToast } from '@/hooks/use-toast';
+import { ToastAction } from '@/components/ui/toast';
+import { config } from "@/lib/config";
+import auth from '@/api/auth';
+import { credentials } from '@/auth/credentials';
 
 export default function CustomersPage() {
+  const { toast } = useToast();
   const sortedCountries = mapData.objects.world.geometries.sort((a, b) => a.properties.name.localeCompare(b.properties.name));
+
   const [customersPage, setCustomersPage] = useState<Page<Customer>>({
     index: 0,
     size: 15,
@@ -23,12 +30,46 @@ export default function CustomersPage() {
   });
   const [numberOfCustomers, setNumberOfCustomers] = useState<number>(temporaryCustomers.length);
   const [newCustomer, setNewCustomer] = useState<Partial<Customer>>();
+  const [photo, setPhoto] = useState<File | null>(null);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   const fieldContainerStyle = "flex flex-wrap sm:grid sm:grid-cols-4 items-center gap-4 p-1";
 
-  useEffect(() => {
-    console.log(newCustomer);
-  }, [newCustomer]);
+  const handleSaveChanges = async () => {
+    if (!newCustomer) return;
+    const form = new FormData();
+
+    const tokens = credentials.get();
+    if (auth && !tokens.access) return null;
+
+    form.append("photo", photo as Blob);
+    for (const key in newCustomer) {
+      form.append(key, newCustomer[key as keyof(typeof newCustomer) ]! as string);
+    }
+    console.log(form);
+    const response = await fetch(`${config.api.url}/customers`, {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        Authorization: `Bearer ${tokens.access}`,
+      },
+      body: form
+    })
+    console.log(response);
+    if (response && response.ok) {
+      setOpenDialog(false);
+      toast({
+        title: "New customer created",
+        description: `${newCustomer.name} ${newCustomer.surname} has been successfully created.`,
+      })
+    } else {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        action: <ToastAction altText="Try again" onClick={handleSaveChanges}>Try again</ToastAction>,
+      })
+    }
+  }
 
   return (
     <main>
@@ -41,8 +82,15 @@ export default function CustomersPage() {
           </h3>
         </div>
         <div>
-          <Dialog>
-            <DialogTrigger asChild className="px-2.5">
+          <Dialog
+            open={openDialog}
+            onOpenChange={setOpenDialog}
+          >
+            <DialogTrigger
+              asChild
+              className="px-2.5"
+              onClick={() => setOpenDialog(true)}
+            >
               <Button variant="default">
                 <PlusIcon className="w-5 h-5" />
               </Button>
@@ -208,10 +256,36 @@ export default function CustomersPage() {
                       </SelectContent>
                     </Select>
                   </div>
+                  <div className={fieldContainerStyle}>
+                    <Label htmlFor="photo" className="text-right">
+                      Photo
+                    </Label>
+                    <Input
+                      id="photo"
+                      type="file"
+                      className="col-span-3"
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        const file = event.target.files?.[0];
+                        if (file) {
+                          setPhoto(file);
+                          const format = file.type.split('/')[1] as PhotoFormat;
+                          setNewCustomer(prev => ({
+                            ...prev,
+                            photoFormat: format,
+                          }));
+                        }
+                      }}
+                    />
+                  </div>
                 </div>
               </ScrollArea>
               <DialogFooter>
-                <Button type="submit">Save changes</Button>
+                <Button
+                  type="submit"
+                  onClick={handleSaveChanges}
+                >
+                  Save changes
+                </Button>
               </DialogFooter>
             </DialogContent>
           </Dialog>
