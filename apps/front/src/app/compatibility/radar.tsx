f0252c384adfddfffef015c4407a8acaddb6213b
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-09-11 10:45:56 +0200

    feat: Add zodiac signs in compatibility page

--- apps/front/src/app/compatibility/radar.tsx
+++ apps/front/src/app/compatibility/radar.tsx
@@ -1,58 +1,48 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import {
-    PolarAngleAxis,
-    PolarGrid,
-    Radar as SRadar,
-    RadarChart,
+  PolarAngleAxis,
+  PolarGrid,
+  Radar as SRadar,
+  RadarChart,
 } from "recharts";
-
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
 } from "@/components/ui/chart";
 
 const chartConfig = {
-    desktop: {
-        label: "Radar",
-        color: "hsl(var(--primary))",
-    },
+  desktop: {
+    label: "Radar",
+    color: "hsl(var(--primary))",
+  },
 } satisfies ChartConfig;
 
 export function Radar({ data }: { data: { label: string; score: number }[] }) {
-    return (
-        <ChartContainer
-            config={chartConfig}
-            className="mx-auto aspect-square min-h-[250px] min-w-[250px]"
-        >
-            <RadarChart data={data}>
-                <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent hideLabel />}
-                />
-                <PolarGrid gridType="circle" />
-                <PolarAngleAxis dataKey="label" />
-                <SRadar
-                    dataKey="score"
-                    fill="var(--color-desktop)"
-                    fillOpacity={0.6}
-                    dot={{
-                        r: 4,
-                        fillOpacity: 1,
-                    }}
-                />
-            </RadarChart>
-        </ChartContainer>
-    );
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square min-h-[250px] z-[1] min-w-[250px] bg-white rounded-full"
+    >
+      <RadarChart data={data}>
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <PolarGrid gridType="circle" />
+        <PolarAngleAxis dataKey="label" />
+        <SRadar
+          dataKey="score"
+          fill="var(--color-desktop)"
+          fillOpacity={0.6}
+          dot={{
+            r: 4,
+            fillOpacity: 1,
+          }}
+        />
+      </RadarChart>
+    </ChartContainer>
+  );
 }
